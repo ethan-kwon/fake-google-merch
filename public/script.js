95518b4bc59156e4e5ef1f04bd518f929965265a
ethan-kwon <ethan200304@gmail.com>
2021-07-23 18:47:31 +0000

    finish cart system

--- public/script.js
+++ public/script.js
@@ -1,51 +1,128 @@
-const openNav = document.querySelector(".hamburger");
-const closeNav = document.querySelector(".nav__close");
-const nav = document.querySelector(".nav__list");
+const addToCart = document.querySelector("#cart");
 
-openNav.addEventListener("click", () => {
-  const navLeft = nav.getBoundingClientRect().left;
-  if (navLeft < 0) {
-    nav.style.left = "0";
-    document.body.classList.add("active");
-  }
-});
+const cartClicked = () => {
+  console.log("Cart clicked");
+}
 
-closeNav.addEventListener("click", () => {
-  const navLeft = nav.getBoundingClientRect().left;
-  if (navLeft === 0) {
-    nav.style.left = "-40rem";
-    document.body.classList.remove("active");
-  }
-});
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
+}
 
-// Smooth Scroll
-const scrollLinks = [...document.querySelectorAll(".scroll-link")];
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    for (var i = 0; i < removeCartItemButtons.length; i++) {
+        var button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
 
-scrollLinks.forEach(link => {
-  link.addEventListener("click", e => {
-    const id = e.target.getAttribute("href").slice(1);
-    const element = document.getElementById(id);
-    const position = element.offsetTop;
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for (var i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
 
-    window.scrollTo({
-      left: 0,
-      top: position,
-    });
-    nav.style.left = "-40rem";
-    document.body.classList.remove("active");
-  });
-});
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for (var i = 0; i < addToCartButtons.length; i++) {
+        var button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+function purchaseClicked() {
+    alert('Thank you for your purchase')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (var i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText == title) {
+            alert('This item is already added to the cart')
+            return
+        }
+    }
+    var cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>`
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
+}
 
 // Buy Item
-const buy = (amountToPurchase) => {
+const buy = () => {
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for (let i = 0; i < cartItemNames.length; i++) {
+        console.log(cartItems[i])
+    }
     const itemRef = firebase.database().ref();
     var updates = {};
     itemRef.once('value', (snapshot) => {
         const data = snapshot.val();
         for (let key in data) {
             if (data[key].product === "shirt") {
-                if (data[key].quantity - amountToPurchase >= 0) {
-                    updates['quantity'] = data[key].quantity - amountToPurchase;
+                if (data[key].quantity - 1 >= 0) {
+                    updates['quantity'] = data[key].quantity - 1;
                     firebase.database().ref().child(key).update(updates);
                 } else {
                     return
